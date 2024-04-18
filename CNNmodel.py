bc03f1c2ffbc0c2ee756cc5eddf01f85e6d211f0
Jayden Smith <jaydenpsmith@yahoo.com>
2024-04-17 22:39:47 -0400

    Fixed the LPC model and got some decent accuracy on the training set.

--- CNNmodel.py
+++ CNNmodel.py
@@ -96,7 +96,6 @@ def trainModelSpectrogram(baseFolder):
                data = dsData[n,m,:]
                data = SFT.stft(data)
                data = data/np.min(np.abs(data[data!=0]))
-               #SFTData[n,m,:,:] = np.abs(data[:,:int(np.ceil(maxlength/hop))])
                SFTData[n,m,:,:] = 20*np.log10(np.abs(data[:,:int(np.ceil(maxlength/hop))]))
                if False: # Make true to display spectrograms of each word recording
                     fig1, ax1 = plt.subplots(figsize=(6., 4.))  # enlarge plot a bit
@@ -168,55 +167,55 @@ def trainModelSpectrogram(baseFolder):
 
      model = models.Sequential()
      model.add(layers.Input((X.shape[0], X.shape[1], 1)))
+     model.add(layers.BatchNormalization())
      model.add(layers.GaussianNoise(0.1))
      model.add(layers.Conv2D(16, (7,7),activation='relu'))
-     model.add(layers.BatchNormalization())
+     # model.add(layers.BatchNormalization())
      model.add(layers.Dropout(0.25))
      model.add(layers.MaxPooling2D((2,2)))
      model.add(layers.Conv2D(32,(5,5), activation='relu'))
-     model.add(layers.BatchNormalization())
+     # model.add(layers.BatchNormalization())
      model.add(layers.Dropout(0.25))
      model.add(layers.MaxPooling2D((2,2)))
      model.add(layers.Conv2D(64,(5,5), activation='relu'))
-     model.add(layers.BatchNormalization())
+     # model.add(layers.BatchNormalization())
      model.add(layers.Dropout(0.25))
      model.add(layers.MaxPooling2D((2,2)))
      model.add(layers.Flatten())
      model.add(layers.Dense(nFFT//2, activation='relu'))
-     model.add(layers.BatchNormalization())
+     # model.add(layers.BatchNormalization())
      model.add(layers.Dropout(0.5))
      model.add(layers.Dense(numWords, activation='softmax'))
      model.summary()
 
-     METRICS = metrics=['accuracy']
-               	# Precision(name='precision')]
+     METRICS = metrics=['accuracy',
+               	Precision(name='precision')]
 
      model.compile(optimizer='adam',
                loss='categorical_crossentropy',
                metrics=METRICS)
 
 
-     trainingHistory = model.fit(Xtrain,Ytrain,epochs=30,validation_data=(Xval,Yval))
+     trainingHistory = model.fit(Xtrain,Ytrain,epochs=40,validation_data=(Xval,Yval))
      # testAccuracySpec = model.evaluate(Xtest, Ytest)[1]
-     show_performance_curve(trainingHistory,'accuracy','accuracy')
-     return model, maxlength, numFilesperWord
+     #show_performance_curve(trainingHistory,'accuracy','accuracy')
+     return model, maxlength
 
 def trainModelLPC(baseFolder):
      dsData, numWords, numFilesperWord, maxlength, fs = readData(baseFolder)
      # Linear Prediction
      p = 12 # order, must be even
      N = 100 # length of window
-     offset = 100 # how much to offset the next window by
+     offset = 50 # how much to offset the next window by
 
      w = np.hanning(N) # window values
      maxm = int(np.floor((maxlength-N)/offset))
      Xlpc = np.zeros((numWords*numFilesperWord,p,maxm))
-     # fourth dimension: 0 is magnitude, 1 is phase
 
      for n in range(numWords):
           for m in range(numFilesperWord):
                data = dsData[n,m,:]
-               data = data[abs(data)>1e-9] # Fixes the fact that the data was already zero padded
+               data = data[abs(data)>0] # Fixes the fact that the data was already zero padded
                numm = int(np.floor((len(data)-N)/offset)) # how many windowed time series
                A = np.concatenate((np.ones((1,numm)), np.zeros((p,numm))))
 
@@ -241,54 +240,61 @@ def trainModelLPC(baseFolder):
                Xlpc[n*numFilesperWord+m,:p//2,:] = np.concatenate((np.abs(sortedroots),np.zeros((p//2,maxm-numm))),axis=1)
                Xlpc[n*numFilesperWord+m,p//2:,:] = np.concatenate((np.angle(sortedroots),np.zeros((p//2,maxm-numm))),axis=1)
 
-     Xtrainlpc = np.zeros((numWords*(numFilesperWord-2),Xlpc.shape[1],Xlpc.shape[2]))
+     Xtrainlpc = np.zeros((numWords*(numFilesperWord-1),Xlpc.shape[1],Xlpc.shape[2]))
      Xvallpc = np.zeros((numWords,Xlpc.shape[1],Xlpc.shape[2]))
-     Xtestlpc = np.zeros((numWords,Xlpc.shape[1],Xlpc.shape[2]))
+     # Xtestlpc = np.zeros((numWords,Xlpc.shape[1],Xlpc.shape[2]))
 
-     Ytrainlpc = np.zeros((numWords*(numFilesperWord-2),1))
-     Yvallpc = np.zeros((numWords,1))
-     Ytestlpc = np.zeros((numWords,1))
+     Ytrainlpc = np.zeros((numWords*(numFilesperWord-1)))
+     Yvallpc = np.zeros((numWords))
+     # Ytestlpc = np.zeros((numWords,1))
 
-     Y = np.zeros((numFilesperWord*numWords,1))
-     for i in range(numFilesperWord):
-          Y[i] = 0
-          Y[numFilesperWord+i] = 1
-          Y[numFilesperWord*2+i] = 2
+     Y = np.zeros((numFilesperWord*numWords))
+     for i in range(numWords):
+          for index in range(numFilesperWord):
+               Y[i*numFilesperWord + index] = i
 
      for i in range(numWords):
-          Xtrainlpc[i*(numFilesperWord-2):(i+1)*(numFilesperWord-2),:,:] = Xlpc[i*numFilesperWord:(i+1)*numFilesperWord-2,:,:]
-          Xvallpc[i,:,:] = Xlpc[(i+1)*numFilesperWord-2,:,:]
-          Xtestlpc[i,:,:] = Xlpc[(i+1)*numFilesperWord-1,:,:]
+          Xtrainlpc[i*(numFilesperWord-1):(i+1)*(numFilesperWord-1),:,:] = Xlpc[i*numFilesperWord:(i+1)*numFilesperWord-1,:,:]
+          Xvallpc[i,:,:] = Xlpc[(i+1)*numFilesperWord-1,:,:]
+          # Xtestlpc[i,:,:] = Xlpc[(i+1)*numFilesperWord-1,:,:]
 
-          Ytrainlpc[i*(numFilesperWord-2):(i+1)*(numFilesperWord-2)] = Y[i*numFilesperWord:(i+1)*numFilesperWord-2]
-          Yvallpc[i] = Y[(i+1)*numFilesperWord-2]
-          Ytestlpc[i] = Y[(i+1)*numFilesperWord-1]
+          Ytrainlpc[i*(numFilesperWord-1):(i+1)*(numFilesperWord-1)] = Y[i*numFilesperWord:(i+1)*numFilesperWord-1]
+          Yvallpc[i] = Y[(i+1)*numFilesperWord-1]
+          # Ytestlpc[i] = Y[(i+1)*numFilesperWord-1]
 
      Ytrainlpc = to_categorical(Ytrainlpc,numWords)
      Yvallpc = to_categorical(Yvallpc,numWords)
-     Ytestlpc = to_categorical(Ytestlpc,numWords)
+     # Ytestlpc = to_categorical(Ytestlpc,numWords)
 
      modelLPC = models.Sequential()
-     modelLPC.add(layers.Conv2D(Xlpc.shape[1], (3,3),  activation='relu', input_shape=(Xlpc.shape[1], Xlpc.shape[2], 1)))
-     modelLPC.add(layers.Conv2D(Xlpc.shape[1]*2, (3,3), activation='relu'))
-     modelLPC.add(layers.Conv2D(Xlpc.shape[1]*2, (3,3), activation='relu'))
-     modelLPC.add(layers.Conv2D(Xlpc.shape[1]*2, (3,3), activation='relu'))
+     modelLPC.add(layers.Input((Xlpc.shape[1], Xlpc.shape[2], 1)))
+     modelLPC.add(layers.GaussianNoise(0.05))
+     modelLPC.add(layers.Conv2D(2, (3,3),  activation='relu'))
+     modelLPC.add(layers.Dropout(0.25))
+     modelLPC.add(layers.Conv2D(4, (3,3), activation='relu'))
+     modelLPC.add(layers.Dropout(0.25))
+     modelLPC.add(layers.Conv2D(8, (3,3), activation='relu'))
+     modelLPC.add(layers.Dropout(0.25))
+     modelLPC.add(layers.Conv2D(16, (3,3), activation='relu'))
+     modelLPC.add(layers.Dropout(0.25))
      modelLPC.add(layers.Flatten())
-     modelLPC.add(layers.Dense(Xlpc.shape[1]*2, activation='relu'))
-     modelLPC.add(layers.Dense(Y.shape[0]))
+     modelLPC.add(layers.Dense(32, activation='relu'))
+     modelLPC.add(layers.Dropout(0.25))
+     modelLPC.add(layers.Dense(numWords, activation='softmax'))
      modelLPC.summary()
 
-     modelLPC.compile(optimizer='adam',
-               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-               metrics=['accuracy'])
+     METRICSLPC = metrics=['accuracy',
+               	Precision(name='precision')]
 
-     testAccuracyLPC = 0
-     while testAccuracyLPC < 0.75:
-          modelLPC.fit(Xtrainlpc,Ytrainlpc,epochs=20,validation_data=(Xvallpc,Yvallpc))
-          testAccuracyLPC = modelLPC.evaluate(Xtestlpc, Ytestlpc)[1]
+     modelLPC.compile(optimizer='adam',
+               loss='categorical_crossentropy',
+               metrics=METRICSLPC)
+
+     modelLPC.fit(Xtrainlpc,Ytrainlpc,epochs=40,validation_data=(Xvallpc,Yvallpc))
+     # testAccuracyLPC = modelLPC.evaluate(Xtestlpc, Ytestlpc)[1]
      return modelLPC, maxlength, numFilesperWord
 
-def predictWithSpecModel(model,maxlength,numFilesperWord,filepath):
+def predictWithSpecModel(model,maxlength,filepath):
      data, fs = sf.read(filepath)
      rawData = np.array(data)
      
@@ -296,31 +302,74 @@ def predictWithSpecModel(model,maxlength,numFilesperWord,filepath):
      fs = fs/q
      dsData = decimate(rawData,q)
 
-     if len(dsData) > maxlength:
-          datatrunc = dsData[:maxlength]
-     elif len(dsData) < maxlength:
-          datatrunc = np.concatenate((dsData,np.zeros(maxlength-len(dsData))))
-     else:
-          datatrunc = dsData
-
      # STFT
-     nFFT = 64 # Must be even
-     lenWindow = 50
+     nFFT = 256 # Must be even
+     lenWindow = 256
      w = np.hanning(lenWindow) # Wideband = 40, Narrowband = 240
-     hop = 50
+     hop = 64
      SFT = ShortTimeFFT(w,hop=hop,fs=fs,mfft=nFFT,scale_to='magnitude')
-     SFTdata = SFT.stft(datatrunc)
-     SFTdata = SFTdata/np.min(np.abs(datatrunc[datatrunc!=0]))
+     SFTdata = SFT.stft(dsData)
+     SFTdata = SFTdata/np.min(np.abs(dsData[dsData!=0]))
      SFTdata = 20*np.log10(np.abs(SFTdata[:,:int(np.ceil(maxlength/hop))]))
 
-     prediction = model.predict_on_batch(SFTdata.reshape(1, SFTdata.shape[0], SFTdata.shape[1], 1))
+     prediction = model.predict_on_batch(SFTdata.reshape(1, SFTdata.shape[0], SFTdata.shape[1]))
      labelPrediction = np.argmax(prediction, axis=1)
-     certaintyVal = np.max(prediction, axis=1)
-     return labelPrediction//numFilesperWord, certaintyVal
+     certaintyVal = np.max(prediction, axis=1)[0]
+     return labelPrediction, certaintyVal
 
-modelS, maxlengthS, numFilesperWord = trainModelSpectrogram(baseFolder)
-# modelL, maxlengthL, numFilesperWord = trainModelLPC(baseFolder)
+def predictWithLPCModel(model,maxlength,filepath):
+     data, fs = sf.read(filepath)
+     rawData = np.array(data)
 
-label, val = predictWithSpecModel(modelS,maxlengthS,numFilesperWord,
+     q = 6
+     fs = fs/q
+     dsData = decimate(rawData,q)
+
+     # Linear Prediction
+     p = 12 # order, must be even
+     N = 100 # length of window
+     offset = 50 # how much to offset the next window by
+
+     w = np.hanning(N) # window values
+     maxm = int(np.floor((maxlength-N)/offset))
+     Xlpc = np.zeros((p,maxm))
+
+     numm = int(np.floor((len(dsData)-N)/offset)) # how many windowed time series
+
+     if numm > maxm:
+          numm = maxm
+
+     A = np.concatenate((np.ones((1,numm)), np.zeros((p,numm))))
+
+     for i in range(numm):
+          ym = dsData[offset*i:offset*i+N]*w
+          rm = autocorr(ym)
+          am = solve_toeplitz(rm[0:p],-rm[1:p+1])
+          A[1:,i] = am
+
+     aroots = np.zeros((p,numm),dtype='complex')
+
+     for i in range(numm):
+          aroots[:,i] = np.roots(A[:,i])
+
+     thresh = 0.95
+     aroots[np.abs(aroots) < thresh] = 0
+     aroots[np.angle(aroots) < 0] = 0 # Only keep poles in top half of z-plane
+     order = np.argsort(np.abs(aroots),axis=0)
+     sortedroots = np.flipud(np.take_along_axis(aroots,order,axis=0))
+     sortedroots = sortedroots[:p//2]
+
+     Xlpc[:p//2,:] = np.concatenate((np.abs(sortedroots),np.zeros((p//2,maxm-numm))),axis=1)
+     Xlpc[p//2:,:] = np.concatenate((np.angle(sortedroots),np.zeros((p//2,maxm-numm))),axis=1)
+
+     prediction = model.predict_on_batch(Xlpc.reshape(1, Xlpc.shape[0], Xlpc.shape[1], 1))
+     labelPrediction = np.argmax(prediction, axis=1)
+     certaintyVal = np.max(prediction, axis=1)[0]
+     return labelPrediction, certaintyVal
+
+# modelS, maxlengthS = trainModelSpectrogram(baseFolder)
+modelL, maxlengthL, numFilesperWord = trainModelLPC(baseFolder)
+
+label, val = predictWithLPCModel(modelL,maxlengthL,
                               'C:\\Users\\jayde\\School Stuff\\DSP of Speech Signals\\Project\\vocab-recognition\\DATASET17_6\\Thermodynamic\\Thermodynamic_4.wav')
 print(f'Label={label}, Val={val}')
